549bdcf7750975e324f20af4459987ddea038c81
hxy2006 <79700623+hxy2006@users.noreply.github.com>
2023-08-24 06:13:27 +0800

    Add files via upload

---
+++ test1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    printf("%d\n", 100);
+    printf("%d\n", sizeof(char));
+    printf("%d\n", sizeof(short));
+    printf("%d\n", sizeof(int));
+    printf("%d\n", sizeof(long));
+    printf("%d\n", sizeof(long long));
+    printf("%d\n", sizeof(float));
+    printf("%d\n", sizeof(double));
+    system("pause");
+    return 0;
+}
---
+++ test2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int age = 20;
+    double weight = 75.3;
+    age = age+1;
+    weight = weight-10;
+    printf("%d\n", age);
+    printf("%lf\n", weight);
+    return 0;
+}
